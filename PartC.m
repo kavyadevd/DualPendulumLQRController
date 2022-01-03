a82f33929fe837a0270ef30907f913559ba648b9
Kavyashree <kavya27.devadiga@gmail.com>
2022-01-02 21:22:11 -0500

    Update PartC.m

--- PartC.m
+++ PartC.m
@@ -8,11 +8,11 @@ A=[0 1 0 0 0 0;
     0 0 -(m1*g)/(M*l2) 0 -(g*(M+m2))/(M*l2) 0];
 
 
-%declaring the B matrix
+% Matrix B
 B=[0; 1/M; 0; 1/(M*l1); 0; 1/(M*l2)];
 
 
-disp("Controllability matrix =");
+disp("Controllability Matrix =");
 CTRB = [B A*B (A^2)*B (A^3)*B (A^4)*B (A^5)*B];
 Ct = det(CTRB);
 disp("Finding the determinant of the controllability matrix=");
